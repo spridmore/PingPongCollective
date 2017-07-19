174022f5aea1cf89f067b9d47cdde2fe17ce1e1c
spridmore <spridmore@gmail.com>
2017-07-19 11:11:41 -0700

    Deleted empty service

---
+++ controllers/standingsController.js
@@ -0,0 +1,25 @@
+'use strict;'
+
+angular
+  .module("pingPongApp")
+  .controller("standingsController", function ($scope, standingsService, playersService) {
+
+    $scope.players = playersService.getPlayers();
+
+    $scope.logInfo = function() {
+      playersService.testPlayer($scope.player1, $scope.player2, $scope.scorePlayerOne, $scope.scorePlayerTwo);
+      $scope.scorePlayerOne = "";
+      $scope.scorePlayerTwo = "";
+      $scope.player1 = "";
+      $scope.player2 = "";
+    };
+
+    $scope.sortType = 'firstName';
+    $scope.sortReverse = false;
+
+    $scope.lessThan = function(age, val) {
+      return function(item) {
+        return item[age] < val;
+      };
+    };
+  })
